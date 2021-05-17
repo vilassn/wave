9e2167a29385193efbdaca96a15df4ff02ef5ab1
Vilas <vilassninawe@gmail.com>
2021-05-17 15:44:00 +0530

    updated data

--- wave.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Script to run wave on Mac OS
-
-cat runtime/jre_mac64/Contents/Home/lib/modules_aa runtime/jre_mac64/Contents/Home/lib/modules_ab runtime/jre_mac64/Contents/Home/lib/modules_ac > runtime/jre_mac64/Contents/Home/lib/modules
-runtime/jre_mac64/Contents/Home/bin/java --module-path runtime/jre_mac64/javafx-sdk-16/lib/ --add-modules javafx.controls,javafx.fxml,javafx.web -jar runtime/bin/wave > nul 2>&1 &
